d8911558f34881eba5862d9ae0e27b4a08d66974
Alphius Wambua <alphiuswambua@gmail.com>
2019-10-05 15:34:19 +0300

    Third part Added registration form and Book module with token

--- frontend/frontendapp/src/App.js
+++ frontend/frontendapp/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Login from './components/Login';
+import Books from './components/books';
+
 
 function App() {
+
+  const [token, setToken] = useState('')
+
+  const userLogin = (tok) => {
+    setToken(tok)
+  }
+
   return (
     <div className="App">
-      <Login />
+      <Login  userLogin={userLogin}/>
+      <Books token={token}/>
     </div>
   );
 }
--- frontend/frontendapp/src/components/Login.js
+++ frontend/frontendapp/src/components/Login.js
@@ -8,8 +8,21 @@ class Login extends Component {
     }
 
     login = event => {
-        console.log(this.state.credentials)
         fetch('http://127.0.0.1:8000/auth/', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(this.state.credentials)
+        })
+        .then(data => data.json())
+        .then(
+            data => {
+                this.props.userLogin(data.token)
+            }
+        ).catch( error => console.error(error))
+        
+    }
+    register = event => {
+        fetch('http://127.0.0.1:8000/api/users/', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(this.state.credentials)
@@ -46,6 +59,8 @@ class Login extends Component {
                 </label>
                 <br/>
                 <button onClick={this.login}>Login</button>
+                <button onClick={this.register}>Register</button>
+
             </div>
         );
         }
